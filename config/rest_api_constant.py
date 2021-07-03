343d59b1fde969bcfd3e0131725fd4122da1d343
thanhnv <nguyenthanh2303@gmail.com>
2021-07-03 14:12:57 +0700

    feat: add route estimate for next n days

--- config/rest_api_constant.py
+++ config/rest_api_constant.py
@@ -1,6 +1,7 @@
 class RouteApiConstant:
     get_credit_score_path = "/CreditScore"
     estimate_credit_score_path = "/EstimateCreditScore"
+    estimate_credit_score_n_days_path = "/EstimateCreditScoreNDays"
     update_credit_score_path = "/UpdateCreditScore"
     visualize_data_sample_path = "/VisualizeDataSample"
     get_token_credit_score_path = "/TokenCreditScore"
